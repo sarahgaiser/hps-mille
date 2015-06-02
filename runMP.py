916eaf5251f279f640fa15ce6d455b152c084540
perhansson <per.hansson80@gmail.com>
2015-06-02 12:58:12 -0700

    Add possibility of using module names instead of MP parameters as input.

--- runMP.py
+++ runMP.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python
-import argparse, subprocess, sys, os.path
+import argparse, subprocess, sys, os.path, re
 
 pedeBin = 'MillepedeII/pede'
+paramMapFile = 'hpsSvtParamMap.txt'
+global paramMap
+paramMap= {}
 
 class Parameter:
     def __init__(self, i,val,active,error=None):
         self.i = i
+        self.name = getSensorName(i)
         self.val = val
         self.active = active
         self.error=error
@@ -23,7 +27,34 @@ class Parameter:
         s = '%5d %10f %10f' % (self.i, self.val, self.active)
         if self.error!=None:
             s += '    %f' % self.error
+        s += ' %s' % self.name
         return s
+
+
+def initParamMap():
+    try:
+        f = open(paramMapFile,'r')
+    except IOError:
+        print 'Cannot open ', paramMapFile
+        sys.exit(1)
+    else:
+        for line in f.readlines():
+            if 'MilleParameter' in line:
+                continue
+            paramMap[int(line.split()[0])] = line.split()[1]
+        print 'Initialized param map with ', len(paramMap), ' parameters'
+        #for k,v in paramMap.iteritems():
+        #    print k,' ', v
+        f.close()
+
+def getSensorName(i):
+    if not bool(paramMap):
+        initParamMap()        
+    if i in paramMap:
+        return paramMap[i]
+    else:
+        print 'Cannot find sensor for param ', i
+        sys.exit(1)
 
 
 def getMinimStr(filename):
@@ -84,6 +115,64 @@ def updateFloatParams(pars,floats):
         pfound.active = 0
     return
 
+
+def getModuleNrFromDeName(deName):
+    m = re.search("module_L(\d)\S_", deName)
+    if m==None:
+        print 'Wrong module name format ', module
+        sys.exit(1) 
+    else:
+        l = m.group(1)
+        return int(l)
+
+
+def getDir(param):
+    i = int((param%1000)/100.0)
+    if i==1:
+        return 'u'
+    elif i==2:
+        return 'v'
+    elif i==3:
+        return 'w'
+    else:
+        print 'cannot extract dir from param ', param
+        sys.exit(1)
+def getType(param):
+    return int((param%10000)/1000.0)
+def getHalf(param):
+    i = int((param%100000)/10000.0)
+    if i==1:
+        return 't'
+    elif i==2:
+        return 'b'
+    else:
+        print 'cannot extract half from param ',param
+        sys.exit(1)
+
+
+def getParamsFromModule(module):
+    params = []       
+    m = re.search("L([1-6])([tb])_([uvw])", module)
+    if m==None:
+        print 'Wrong module name format ', module, '. Should be e.g. \'L4t_u\''
+        sys.exit(1) 
+    else:
+        moduleNr = int(m.group(1))
+        half = m.group(2)
+        d = m.group(3)
+        #print 'find param for ', module, ' ', moduleNr, ' ' , half, ' ' , d
+        for k,v in paramMap.iteritems():
+            loopNr = getModuleNrFromDeName(v)
+            loopType = getType(k)
+            loopHalf = getHalf(k)
+            #print 'test ', k, ' ', loopNr, ' ', loopType, ' ', loopHalf
+            # only u-dir translation for now
+            if moduleNr == loopNr and getDir(k)==d and loopType==1 and half==loopHalf:
+                params.append(k)
+        if not bool(params):
+            print 'Cannot find param  for module ', module
+    return params
+
 def updateParams(pars,otherparms,resetActive=True):
     print 'There are ',len(otherparms),' to update'
     parsnew = []
@@ -139,7 +228,15 @@ def printResResults():
                     p  = Parameter.fromstr(l)
                     if p.active != -1.0:
                         delta = float(l.split()[3])
-                        print "%s change is %f or %.1f percent" % ( p.toString(), delta, delta/(p.val-delta)*100.0)
+                        if delta==0.0:
+                            print "%s NO change" % ( p.toString() )
+                        else:
+                            d = p.val-delta
+                            if d==0.0:
+                                print "%s change is %f " % ( p.toString(), delta)
+                            else:
+                                print "%s change is %f or %f percent" % ( p.toString(), delta, delta/(d)*100.0)
+            
             if 'Parameter' in l:
                 active=True
     return
@@ -197,12 +294,17 @@ def main(args):
     # normally require that they become inactive here
     pars = updateParams(pars,inputpars,True)
 
-    # find the floating parameter for this fit
-    if args.float==None:
-        print 'ERROR: need to supply at least 1 parameter to be floated.'
-        return 1
+    floatingPars = []
 
-    updateFloatParams(pars,args.float)
+    # find the floating parameter for this fit
+    if args.Modules is not None:
+        for m in args.Modules:
+            floatingPars.extend([str(mp) for mp in getParamsFromModule(m)])
+    
+    if args.float is not None:
+        floatingPars.extend(args.float)
+
+    updateFloatParams(pars,floatingPars)
 
     print 'List of floating parameters:'
     nfloats = 0
@@ -240,7 +342,8 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='MP help script')
     parser.add_argument('-i','--inputfile', required=True, help='List of parameters to float')
-    parser.add_argument('-f','--float', nargs='+', help='List of parameters to float')
+    parser.add_argument('-f','--float', nargs='+', help='List of MP parameters to float')
+    parser.add_argument('-M','--Modules', nargs='+', help='List of modules to float, e.g. L3b L5b')
     parser.add_argument('-p','--parameters', help='Default parameters')
     parser.add_argument('-m','--minimization', default='steering/steer_minimization_template.txt', help='Default minimization settings')
     parser.add_argument('-d','--debug', action='store_true', help='Debug flag')
