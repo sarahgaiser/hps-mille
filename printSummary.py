cfcf4beea563056afd7e01012b513c8130ee4a80
perhansson <per.hansson80@gmail.com>
2015-09-28 15:11:17 -0700

    try to improve the binning

--- printSummary.py
+++ printSummary.py
@@ -4,18 +4,31 @@ import argparse, subprocess, sys, os.path, re
 import utils
 sys.path.append('pythonutils')
 import compareRootHists
-from ROOT import TGraph, TCanvas, TH1F, TLegend, gPad
+from ROOT import TGraph, TCanvas, TH1F, TLegend, gPad, TH2F
 
 
 def setBinLabels(gr,pars):
+    #h = gr
     h = gr.GetHistogram()
-    print h.GetNbinsX(), ' vs ', gr.GetN()
-    i = 0
+    print 'setBinLabels ', len(pars)
+    ip = 1
     for p in pars:
-        b = h.FindBin(i)
+        b = h.FindBin(ip)
+        print 'ip ', ip, ' b ', b ,': ' , p.toNiceString()
         h.GetXaxis().SetBinLabel(b, p.name + '(' + str(p.i) + ')')
-        i = i + 1
-    
+        ip = ip + 1
+
+def setBinLabelsHist(gr,pars):
+    h = gr
+    #h = gr.GetHistogram()
+    print 'setBinLabels ', len(pars)
+    ip = 0
+    for p in pars:
+        b = h.GetXaxis().FindBin(ip)
+        print 'ip ', ip, ' b ', b ,': ' , p.toNiceString()
+        h.GetXaxis().SetBinLabel(b, p.name + '(' + str(p.i) + ')')
+        ip = ip + 1
+
 
 def plotCmp(filenames):
     fname = filenames[0]
@@ -39,10 +52,13 @@ def plotResCmp(filenames):
     leg.SetFillStyle(0)
     leg.SetFillColor(0)
     savename = ''
+    h_sum_template = None
+    h_sumNZ_template = None
     for filename in filenames:
-        if re.match(args.reject,filename) != None:
-            print 'skip file ', filename, ' base on pattern ', args.reject
-            continue
+        if args.reject != None:
+            if re.match(args.reject,filename) != None:
+                print 'skip file ', filename, ' base on pattern ', args.reject
+                continue
         if savename=='':
             savename = os.path.splitext(os.path.basename(filename))[0]
         run = utils.getRunNr(filename)
@@ -55,10 +71,14 @@ def plotResCmp(filenames):
         h_sum = TGraph()
         h_sumNZ = TGraph()
         i = 0
+        maxVal =  0.09
+        minVal = -0.09
         for p in pars:
             #print binmap[p.name], ' ', p.val
             h_sum.SetPoint(i,i,p.val)
             i = i + 1
+            #if p.val > maxVal: maxVal = p.val
+            #if p.val < minVal: minVal = p.val
         i = 0
         for p in parsNZ:
             h_sumNZ.SetPoint(i,i,p.val)
@@ -73,23 +93,32 @@ def plotResCmp(filenames):
         h_sumNZ.SetMarkerColor(icolor)
         if len(vals)==0:
             c_sum.cd()
-            h_sum.Draw('APL')
+            h_sum_template = TH2F('h_'+h_sum.GetName(),'h_'+h_sum.GetName(),h_sum.GetN(),0,h_sum.GetN(),10,minVal,maxVal)
+            h_sum_template.SetStats(False)
+            h_sum_template.Draw('hist')
+            h_sum_template.SetTitle('Millepede corrections per sensor;;local translation/rotations (mm/rad)')
+            h_sum.Draw('PL,same')
             c_sumNZ.cd()
-            h_sumNZ.Draw('APL')
+            h_sumNZ_template = TH2F('h_'+h_sumNZ.GetName(),'h_'+h_sumNZ.GetName(),h_sumNZ.GetN(),0,h_sumNZ.GetN(),10,h_sumNZ.GetMinimum(),h_sumNZ.GetMaximum())
+            h_sumNZ_template.SetStats(False)
+            h_sumNZ_template.Draw('hist')
+            h_sumNZ.Draw('PL,same')
             h_sum.SetTitle('Millepede corrections per sensor;;local translation/rotations (mm/rad)')
             h_sumNZ.SetTitle('Millepede corrections per sensor;;local translation/rotation (mm/rad)')
-            setBinLabels(h_sum,pars)
-            setBinLabels(h_sumNZ,parsNZ)
+            setBinLabelsHist(h_sum_template,pars)
+            setBinLabelsHist(h_sumNZ_template,parsNZ)
         else:
             c_sum.cd()
             h_sum.Draw('PL,same')
             c_sumNZ.cd()
             h_sumNZ.Draw('PL,same')
+
         icolor=icolor+1
         leg.AddEntry(h_sum,'%s'%run,'LP')        
         vals[filename] = h_sum
         valsNZ[filename] = h_sumNZ
     c_sum.cd()
+    c_sum.Update()
     leg.Draw()
     c_sumNZ.cd()
     leg.Draw()
